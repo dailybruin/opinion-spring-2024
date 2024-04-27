ff67416548f851560b4d90e92738929adb3b0ca4
Michelle Kim <mkheeseo@gmail.com>
2024-04-27 14:01:20 -0700

    Initial setup of article grid (title/squiggles)
    • Title resizes, squiggle images do not
    • Need Article Card section done to display grid
    
    Co-Authored-By: katherineli29 <117778634+katherineli29@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import styled from 'styled-components';
 import './App.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import DesktopArticleGrid from "./components/DesktopArticleGrid";
+import MobileArticleGrid from "./components/MobileArticleGrid";
 
 function App() {
   const [ data, setData ] = useState(null);
@@ -17,6 +19,7 @@ function App() {
     <div className="App">
       <Header/>
       Hello Daily Bruin!
+      <DesktopArticleGrid/>
       <Footer/>
     </div>
   );
