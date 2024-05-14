a731acf0fe02215d11b03cb8039bb7baac33f121
Maria Campo Martins <mariacampomartins@gmail.com>
2024-05-13 18:15:45 -0700

    Text resizes and stays centered with window size

--- src/components/Playlist.js
+++ src/components/Playlist.js
@@ -5,6 +5,7 @@ import playlist_right from '../images/playlist-right.png'
 import playlist_temp from '../images/playlist-temp.png'
 
 const Container = styled.div`
+margin-top: -300pt;
 position: relative;
 height: 2000px;
 width: 100%;
@@ -12,6 +13,7 @@ width: 100%;
 const Curve = styled.div`
 float: left;
 position: absolute;
+width: 100%;
 img {
     width: 25%;
     float: left;
@@ -23,24 +25,24 @@ display: flex;
 justify-content: center;
 position: absolute;
 width: 100%;
-top: 780px;
+top: 500px;
 `
 const Text = styled.div`
 color: #F0E8CE;
 margin-top: 30px;
 font-family: "Poppins", sans-serif;
 font-weight: 600;
-font-size: 64px;
+font-size: 10vh;
 `
 
 const Playlist_content = styled.div`
 position: absolute;
-top: 1000px;
+top: 700px;
 width: 100%;
 display: flex;
 justify-content: center;
 img {
-    width: 80%;
+    width: 60%;
 }
 `
 const Playlist = () => {
@@ -50,11 +52,11 @@ const Playlist = () => {
         <img class="curve" src={left_curve} />
         </Curve>
         <Title>
-            <img src={playlist_left}/>
+            <img src={playlist_left} class="left"/>
             <Text>
                 PLAYLIST
             </Text>
-            <img src={playlist_right}/>
+            <img src={playlist_right} class="right"/>
         </Title>
         <Playlist_content>
             <img src={playlist_temp}/>
