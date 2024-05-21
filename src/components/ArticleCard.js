151ba5ca13888a9e14974045fe3b41709be27b89
tsakshi01 <tsakshi01@gmail.com>
2024-05-20 18:29:10 -0700

    Article Card Text Update

--- src/components/ArticleCard.js
+++ src/components/ArticleCard.js
@@ -28,40 +28,35 @@ const Container = styled.div`
                 return `url(${Orange})`;
         }
     }};
+    
 `
 const Title = styled.div `
     height: 81px;
     width: 450px;
-    line-height: 81px;
-    box-sizing: border-box;
     font-weight: 400;
     font-size: 24px;
     text-align: center;
-    overflow: hidden;
     font-family: "Caveat", cursive;
-    position: absolute;
-    top: 70px;
-    left: 60px;
-    
+    position: relative;
+    color: #051626;
+    top: -4em;
+    left: 3em;
 `
 const ByLine = styled.div`
-    width: 450px;
-    height: 45px;
     color: #F0E8CE;
     font-family: "Poppins", sans-serif;
     font-weight: 600;
     font-size: 16px;
     text-align: center;
-    position: absolute;
-    left: 70px;
-    top: 255px;
-    
+    position: relative;
+    top: 3.1em;
+    left: -12em;
 `
 const ArticleCard = (props) => {
     return(
         <Container color={props.article_color}>
-            <Title>{props.article_title}</Title>
-            <ByLine>{props.article_byline}</ByLine>
+            <Title>Title{props.article_title}</Title>
+            <ByLine>BY JOE BRUIN{props.article_byline}</ByLine>
         </Container>
     )
 }
